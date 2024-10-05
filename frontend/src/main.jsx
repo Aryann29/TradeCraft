b9d1abeaa0bd172f5beb3b65e9a7815dca0bdd21
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-10-05 19:22:45 +0530

    Index.js File Update ROute Info

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,36 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard from './components/Dashboard.jsx'
+import Trade from './components/Trade.jsx'
+import Funds from './components/Funds.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/Dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/Funds",
+    element: <Funds />,
+  }, {
+    path: "/Trade",
+    element: <Trade />,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
+
+
