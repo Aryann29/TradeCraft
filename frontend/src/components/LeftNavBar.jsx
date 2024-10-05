d9c73780b3d223f3e86a8872108696a62fb8891e
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-10-05 19:29:02 +0530

    LeftNavBar

---
+++ frontend/src/components/LeftNavBar.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+const LeftNavBar = (props) => {
+  return (
+    <div style={props.style}>
+      LeftNavBar
+      LeftNavBar
+    </div>
+  )
+}
+LeftNavBar.propTypes = {
+    style: PropTypes.object,
+};
+export default LeftNavBar
