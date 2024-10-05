0a463749fe29a793697003ecef86b75ce45c7ed7
aryann29 <aryanp2929@gmail.com>
2024-10-05 11:45:35 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  
+  return (
+    <>
+      q
+
+      <div>
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
