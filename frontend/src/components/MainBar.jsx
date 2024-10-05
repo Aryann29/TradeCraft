71c9a71a253425e059385608b833e57faa05d81c
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2024-10-05 19:29:15 +0530

    MainBar

---
+++ frontend/src/components/MainBar.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const MainBar = (props) => {
+  return (
+    <div style={props.style}>
+MainBar
+MainBar    </div>
+  )
+}
+MainBar.propTypes = {
+    style: PropTypes.object,
+};
+export default MainBar
