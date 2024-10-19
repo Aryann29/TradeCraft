a69046fdd52ea4c4c0e893c3d9c1720c6b0459d3
aryann29 <aryanp2929@gmail.com>
2024-10-19 12:04:13 +0530

    auth url

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/auth';
+const API_URL = 'https://trade-craft-backend.vercel.app/api/auth';
 
 
 const axiosInstance = axios.create({
